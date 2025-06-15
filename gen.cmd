1b38280fb78b62bb3bc36dcea992556d8a22eaae
Austin Wise <AustinWise@gmail.com>
2025-06-15 11:02:09 -0700

    Addjust file location to match namespace.

--- gen.cmd
+++ gen.cmd
@@ -6,6 +6,6 @@ cd /d %~dp0
 
 dotnet tool run ClangSharpPInvokeGenerator -f include\biscuit_auth.h ^
     --namespace us.awise.biscuits.generated ^
-    -o biscuit-csharp\generated.cs ^
+    -o biscuit-csharp\generated\generated.cs ^
     -x c -l biscuit_auth ^
     --with-access-specifier *=Internal
